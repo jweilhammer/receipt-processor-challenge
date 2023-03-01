27baa2caee128c1b16798112483c7696832be44e
Jake Weilhammer <jake.weilhammer@gmail.com>
2023-02-28 21:45:53 -0500

    Setup with Spring boot intializer

---
+++ src/test/java/com/fetch/jake/receiptprocessor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetch.jake.receiptprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
