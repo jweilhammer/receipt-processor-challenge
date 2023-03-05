573cc6a463a8ebf9d3d33ebade90e43d98d89c56
Jake Weilhammer <jake.weilhammer@gmail.com>
2023-03-05 11:30:55 -0500

    Added integration test with DynamoDB via testcontainers

--- src/test/java/com/fetch/jake/receiptprocessor/ApplicationTests.java
+++ src/test/java/com/fetch/jake/receiptprocessor/ApplicationTests.java
@@ -1,13 +1,33 @@
 package com.fetch.jake.receiptprocessor;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+    @Container
+    static final GenericContainer dynamo =
+            new GenericContainer(DockerImageName.parse("amazon/dynamodb-local"))
+                    .withExposedPorts(8000);
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    public static void checkDbContainer() {
+        assertTrue(dynamo.isRunning());
+        String endpoint = "http://" + dynamo.getHost() + ":" + dynamo.getFirstMappedPort();
+        System.out.println("Dynamo DB container reachable at: " + endpoint);
+        System.setProperty("aws.dynamodb.endpoint", endpoint);
+        System.setProperty("integration.tests", "enabled");
+    }
 
+    @Test
+    void contextLoads() {
+    }
 }
